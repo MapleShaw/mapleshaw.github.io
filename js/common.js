830c82aae70bc810ee889127809eafa28ae3a43a
MapleShaw <mapleshaw@gmail.com>
2014-06-12 16:59:51 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@
   $('.row').each(function(index) {
     var _this = $(this);
     if(reg1.test(index/2)){
-      var newNode = _this.find('div.col-md-5');
+      var newNode = _this.find('div.col-md-5');console.log(newNode);
       _this.prepend(newNode);
       _this.find('div.col-md-5').remove();
     }
