0b462992e597d1a0cc16c12fdf4c3c3c8c78d639
MapleShaw <mapleshaw@gmail.com>
2014-06-12 17:17:38 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@
     var html = '';
     if(reg1.test(index/2)){
       var newNode = _this.find('div.col-md-5');console.log(newNode.find('img').attr('src'));
-      html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'++'" alt="Generic placeholder image"></div>';
+      //html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'++'" alt="Generic placeholder image"></div>';
       _this.prepend(html);
       _this.find('div.col-md-5').remove();
     }
