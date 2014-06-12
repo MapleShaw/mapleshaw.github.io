96babf77ca5e9e3b19b50e9c8e7d5c1c7efe258b
MapleShaw <mapleshaw@gmail.com>
2014-06-12 17:11:33 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@
     if(reg1.test(index/2)){
       var newNode = _this.find('div.col-md-5');
       html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'+newNode.find('img').attr('src')+'" alt="Generic placeholder image"></div>';
-      _this.prepend('html');
+      _this.prepend(html);
       _this.find('div.col-md-5').remove();
     }
   });
