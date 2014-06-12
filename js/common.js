d9604b3035fa989c580fc758da14a80e75e5b033
MapleShaw <mapleshaw@gmail.com>
2014-06-12 17:15:11 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
   var reg1 =  /^\d+$/;
   $('.row').each(function(index) {
     var _this = $(this);
-    var html;
+    var html = '';
     if(reg1.test(index/2)){
       var newNode = _this.find('div.col-md-5');console.log(newNode.find('img').attr('src'));
       html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'++'" alt="Generic placeholder image"></div>';
