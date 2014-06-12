16b31cb31b275a34d0a511f829d0bdb5ad4408f6
MapleShaw <mapleshaw@gmail.com>
2014-06-12 17:09:54 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -2,9 +2,11 @@
   var reg1 =  /^\d+$/;
   $('.row').each(function(index) {
     var _this = $(this);
+    var html;
     if(reg1.test(index/2)){
-      var newNode = _this.find('div.col-md-5');console.log(newNode);
-      _this.prepend(newNode);
+      var newNode = _this.find('div.col-md-5');
+      html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'+newNode.find('img').attr('src')+'" alt="Generic placeholder image"></div>';
+      _this.prepend('html');
       _this.find('div.col-md-5').remove();
     }
   });
