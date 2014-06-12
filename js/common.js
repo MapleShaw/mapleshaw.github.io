4a1f04b9ee0527d2723af9d203915fc41f2adc4b
MapleShaw <mapleshaw@gmail.com>
2014-06-12 17:13:53 +0800

    xxx

--- js/common.js
+++ js/common.js
@@ -4,8 +4,8 @@
     var _this = $(this);
     var html;
     if(reg1.test(index/2)){
-      var newNode = _this.find('div.col-md-5');
-      html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'+newNode.find('img').attr('src')+'" alt="Generic placeholder image"></div>';
+      var newNode = _this.find('div.col-md-5');console.log(newNode.find('img').attr('src'));
+      html='<div class="col-md-5"><img class="featurette-image img-responsive"  src="'++'" alt="Generic placeholder image"></div>';
       _this.prepend(html);
       _this.find('div.col-md-5').remove();
     }
