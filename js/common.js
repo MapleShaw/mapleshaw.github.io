b24ec9a5ca5831af73e4aa7c7e68339cd50529f5
MapleShaw <mapleshaw@gmail.com>
2014-06-20 16:31:06 +0800

    iconfont

--- js/common.js
+++ js/common.js
@@ -34,7 +34,7 @@
   // iconArr['css'] = 'icon-css'; 
   // iconArr['html'] = 'icon-html'; 
 
-  $('.posts').find('a').on('hover', function() {alert('1')
+  $('.posts').find('a').on('mouseover', function() {alert('1')
       $(this).addClass('pulse animated');
     });
 
