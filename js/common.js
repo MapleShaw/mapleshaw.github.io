049a68d82831d722e904b3590795e75651d766b7
MapleShaw <mapleshaw@gmail.com>
2014-06-20 16:22:53 +0800

    iconfont

--- js/common.js
+++ js/common.js
@@ -25,4 +25,22 @@
   //   _this.parent().find('li.active').removeClass('active');
   //   _this.addClass('active');
   // });
+
+  /*为url自动添加icon*/
+  // var iconArr = new Array();
+  // iconArr['github'] = 'icon-github';
+  // iconArr['douban'] = 'icon-douban'; 
+  // iconArr['xinlangweibo'] = 'icon-xinlangweibo'; 
+  // iconArr['css'] = 'icon-css'; 
+  // iconArr['html'] = 'icon-html'; 
+
+  $('.posts').each(function() {
+    var _this = $(this);
+    _this.find('a').on('hover', function() {
+      $(this).addClass('pulse animated')
+    });
+  });
+
+  
+
 })(window, jQuery);
