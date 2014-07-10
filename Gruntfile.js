f24fa7a37956b729f2b2f73fb7101a2facd290e7
MapleShaw <mapleshaw@gmail.com>
2014-07-10 14:59:54 +0800

    add new post

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['js/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
---
+++ _posts/2014-07-10-Something About StaticTemplate.md
@@ -0,0 +1,25 @@
+---
+layout: post
+title: Something About StaticTemplate
+intro: 对之前的StaticTemplate做点小思考
+image: http://mapleshaw.github.io/img/post/static.jpg
+---
+
+#Something About StaticTemplate
+
+###package.json文件
+这里面有一点值得一提就是版本号
+
+    "version": "0.0.1" //一定要三位数
+
+
+###重头戏Gruntfile.js
+即使是官方文档也没有对每个插件做详细的说明，大部分是举例子，没有语法解释
+所以到现在很多都还是想当然跟尽可能去尝试，代码太多这里不做赘述，直接看JS文件里面的注释吧
+
+###.gitignore
+才知道这个东东可以把你不想被git上去的文件忽略，叔恶死~
+
+
+###详细的信息
+来自：[我滴Github项目页面](https://github.com/MapleShaw/StaticTemplate)
